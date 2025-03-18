d0826375991ea9654c3a6932d31f56abef775be0
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2025-03-18 11:01:02 +0100

    Update gate.go

--- gate.go
+++ gate.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/TinyBrickBoy/ip-blacklist/plugins/blacklist"
 	"go.minekube.com/gate/cmd/gate"
 	"go.minekube.com/gate/pkg/edition/java/proxy"
 )
 
 func main() {
-    proxy.Plugins = append(proxy.Plugins,
-        blacklist.Plugin,
-    )
-    gate.Execute()
+	fmt.Println("Starting Minekube Gate with IP Blacklist Plugin...")
+	proxy.Plugins = append(proxy.Plugins, blacklist.Plugin)
+	gate.Execute()
 }
